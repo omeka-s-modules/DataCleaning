2e5b2f45d2ed2c445f0c768f531dda2036cf6a6d
Jim Safley <jimsafley@gmail.com>
2020-07-10 18:47:37 -0400

    Improvements

--- src/Form/PrepareAuditForm.php
+++ src/Form/PrepareAuditForm.php
@@ -24,6 +24,19 @@ class PrepareAuditForm extends Form
             ],
         ]);
         $this->add([
+            'type' => ZendElement\Select::class,
+            'name' => 'audit_column',
+            'options' => [
+                'label' => 'Audit column', // @translate
+                'show_required' => true,
+                'value_options' => [
+                    'value' => 'value', // @translate
+                    'uri' => 'uri', // @translate
+                    'value_resource_id' => 'value_resource_id', // @translate
+                ],
+            ],
+        ]);
+        $this->add([
             'type' => OmekaElement\PropertySelect::class,
             'name' => 'property_id',
             'options' => [
@@ -41,39 +54,26 @@ class PrepareAuditForm extends Form
             'name' => 'data_type_name',
             'options' => [
                 'label' => 'Data type', // @translate
+                'empty_option' => '',
                 'show_required' => true,
             ],
             'attributes' => [
-                'value' => 'literal',
                 'class' => 'chosen-select',
                 'data-placeholder' => 'Select a data type', // @translate
             ],
         ]);
-        $this->add([
-            'type' => ZendElement\Select::class,
-            'name' => 'audit_column',
-            'options' => [
-                'label' => 'Audit column', // @translate
-                'show_required' => true,
-                'value_options' => [
-                    'value' => 'value', // @translate
-                    'uri' => 'uri', // @translate
-                    'value_resource_id' => 'value_resource_id', // @translate
-                ],
-            ],
-        ]);
 
         $inputFilter = $this->getInputFilter();
+        $inputFilter->add([
+            'name' => 'audit_column',
+            'required' => true,
+        ]);
         $inputFilter->add([
             'name' => 'property_id',
             'required' => true,
         ]);
         $inputFilter->add([
             'name' => 'data_type_name',
-            'required' => true,
-        ]);
-        $inputFilter->add([
-            'name' => 'audit_column',
             'required' => true,
         ]);
     }
